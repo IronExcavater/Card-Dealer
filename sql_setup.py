48afef7eeb79f336d7502881dc57113e44e8300f
IronExcavater <83278134+IronExcavater@users.noreply.github.com>
2024-04-14 00:47:10 +1000

    Fixed sql_setup with new columns from last commit

--- sql_setup.py
+++ sql_setup.py
@@ -3,7 +3,8 @@ import sql
 create = '''
 CREATE TABLE players (
     user_id VARCHAR(18) PRIMARY KEY NOT NULL,
-    name TEXT NOT NULL,
+    user_name TEXT NOT NULL,
+    display_name TEXT NOT NULL,
     balance INT NOT NULL,
     last_reward DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
 )
